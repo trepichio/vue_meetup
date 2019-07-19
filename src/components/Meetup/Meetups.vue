1fff036918c57d783c448080af19570a27ac3848
trepichio <trepichio@gmail.com>
2019-07-19 19:58:17 -0300

    add static Meetup listing

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,19 +1,48 @@
 <template>
-	<div>Meetups page</div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm10 offset-sm1 md8 offset-md2>
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5 sm4 md5>
+                <v-img
+                  src="https://images.musement.com/cover/0002/49/thumb_148242_cover_header.jpeg?w=1200&h=630&q=60&fit=crop"
+                  height="130px"
+                ></v-img>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title primary-title>
+                  <div>
+                    <h5 class="headline white--text mb-0">
+                      Independence Day Meetup
+                    </h5>
+                    <div>4th July 2019</div>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn flat to="/meetups/1">
+                    <v-icon left>arrow_forward</v-icon>
+                    View Meetup
+                  </v-btn>
+                </v-card-actions>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  name: "Meetups",
 
-  name: 'Meetups',
-
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
