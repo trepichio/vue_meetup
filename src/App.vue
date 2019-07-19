e4eba86feb233ecbe68840e91678e58350d6afbb
trepichio <trepichio@gmail.com>
2019-07-19 19:51:55 -0300

    file linted and added cursor pointer on carousel

--- src/App.vue
+++ src/App.vue
@@ -7,38 +7,34 @@
         >
       <v-list>
         <v-list-tile
-          v-for='item in menuItems'
-          :key='item.title'
-          router :to="item.link"
-          >
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
           <v-list-tile-action>
-            <v-icon>{{item.icon}}</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>{{item.title}}</v-list-tile-content>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
-
-
     </v-navigation-drawer>
-    <v-toolbar class="info">
+    <v-toolbar class="primary">
       <v-toolbar-side-icon
         @click.stop="drawer = !drawer"
-        class="hidden-sm-and-up"></v-toolbar-side-icon>
+        class="hidden-sm-and-up"
+      ></v-toolbar-side-icon>
       <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">MeetUp</router-link>
+        <router-link to="/" tag="span" style="cursor: pointer"
+          >MeetUp</router-link
+        >
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-          v-for='item in menuItems'
-          :key='item.title'
-          router :to="item.link"
-          flat
-        >
-          <v-icon left>{{item.icon}}</v-icon>
-          {{item.title}}
+        <v-btn v-for="item in menuItems" :key="item.title" :to="item.link" flat>
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
-    </v-toolbar-items>
+      </v-toolbar-items>
     </v-toolbar>
     <main>
       <router-view></router-view>
@@ -47,7 +43,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data () {
