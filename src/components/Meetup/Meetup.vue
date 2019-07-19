f42268e9c0f3a022465f5b1b35cf737c094bc525
trepichio <trepichio@gmail.com>
2019-07-19 19:56:04 -0300

    individual meetup component

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-container>
+		<v-layout row wrap>
+			<v-flex xs12>
+				<v-card>
+					<v-card-title>
+						<h2 class="primary--text">My meetup</h2>
+					</v-card-title>
+					<v-img
+					  src="https://images.musement.com/cover/0002/49/thumb_148242_cover_header.jpeg?w=1200&h=630&q=60&fit=crop"
+					  height="400px"
+					></v-img>
+					<v-card-text>
+						<div class="info--text">4th July 2019 - Where its takes place</div>
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn class="primary">Register</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+export default {
+
+  name: 'Meetup',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
